a994d0374ed87245ed617c70781d528eb2d396ae
souliya pps <souliya.litd@gmail.com>
2022-08-21 12:42:51 +0700

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
