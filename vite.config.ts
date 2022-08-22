5f5fe6142347321aa6c353c217825ebc3a83d471
souliya pps <souliya.litd@gmail.com>
2022-08-22 16:12:35 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,39 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import vitePluginImp from "vite-plugin-imp";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import vitePluginImp from 'vite-plugin-imp'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 function pathResolve(dir: string) {
-  return resolve(__dirname, ".", dir);
+  return resolve(__dirname, '.', dir)
 }
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    reactRefresh({
+      // Exclude storybook stories and node_modules
+      exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+      // Only .tsx files
+      include: '**/*.tsx',
+    }),
     vitePluginImp({
       optimize: true,
       libList: [
         {
-          libName: "antd",
-          libDirectory: "es",
-          style: (name) => `antd/es/${name}/style`,
+          libName: 'antd',
+          libDirectory: 'es',
+          style: name => `antd/es/${name}/style`,
         },
       ],
     }),
   ],
-  base: "./",
+  base: './',
   resolve: {
     alias: [
       {
         find: /@\//,
-        replacement: pathResolve("src") + "/",
+        replacement: pathResolve('src') + '/',
       },
     ],
   },
@@ -37,4 +44,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
